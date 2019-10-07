3b4130e12778869f7c5a24d02e4932401803599c
Mikayda Mills <mills.ma12@gmail.com>
2019-10-07 16:52:14 -0400

    add user model

--- models.js
+++ models.js
@@ -20,9 +20,51 @@ if (process.env.DATABASE_URL) {
     }
   });
 }
+
+const User = sequelize.define('user', {
+  first_name: Sequelize.STRING,
+  last_name: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    unique: true, // checks if email is already being used
+    allowNull: false,
+    validate: {
+      isEmail: { // checks if input is in email form
+        msg: 'please enter a valid email address'
+      },
+      notNull: { // message given if null
+        msg: 'please enter an email address'
+      }
+    }
+  },
+  username: {
+    type: Sequelize.STRING,
+    unique: true, // checks if username is already being used
+    allowNull: false,
+    validate: {
+      notNull: { // message given if null
+        msg: 'please enter a username'
+      }
+    }
+  },
+  password_digest: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notNull: { // message given if null
+        msg: 'please enter a password'
+      }
+    }
+  }
+});
+
 const Rock = sequelize.define("rock", {
-  name: Sequelize.STRING,
-  uses: Sequelize.TEXT
+  name: {
+    type: Sequelize.STRING,
+    unique: true, // checks if email is already being used
+    allowNull: false
+  },
+  uses: Sequelize.TEXT,
 });
 
 const Property = sequelize.define("property", {
